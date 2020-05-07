fcd65fedfbbe7c2a62c432688411caf88145099f
Anibal Guerra <guerra15anibal@hotmail.com>
2020-05-07 16:44:06 -0600

    adding jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Danag100921.",
-  database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Danag100921.",
+    database: "burgers_db",
+  });
+}
 
 // Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 module.exports = connection;
